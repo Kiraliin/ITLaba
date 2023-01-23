a5db4121fcd4ef602916b71939531760120e3cd5
Kiraliin <108956447+Kiraliin@users.noreply.github.com>
2023-01-23 15:15:16 +1000

    add tests and classes

---
+++ domain/models/Doctor/Doctors.cs
@@ -0,0 +1,8 @@
+﻿namespace domain.models;
+
+public class Doctor
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public Specialization Specialization { get; set; }
+}
---
+++ domain/models/Schendule/Schendule.cs
@@ -0,0 +1,8 @@
+﻿namespace domain.models;
+
+public class Schendule
+{
+    public int DoctorId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
